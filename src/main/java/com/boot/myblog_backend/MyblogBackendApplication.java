b6cd93c1f928d146fe7c9bd9271dfbc92a582351
gzhgzh <1205702537@qq.com>
2023-02-10 00:24:07 +0800

    first commit

---
+++ src/main/java/com/boot/myblog_backend/MyblogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.boot.myblog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boot/myblog_backend/MyblogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.myblog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
